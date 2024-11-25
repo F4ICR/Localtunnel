35d26a3ea5d9f4f767e8151f94bc475caec721d4
Pascal <55432858+F4ICR@users.noreply.github.com>
2024-11-25 22:35:34 +0100

    Update settings.py

--- settings.py
+++ settings.py
@@ -14,10 +14,10 @@ LOG_FILE = os.path.join(SCRIPT_DIR, "tunnel_output.log")
 
 # Autres variables globales
 PORT = 3000  # Le port local que vous souhaitez exposer
-EMAIL = "pascal.paquet@gmail.com"  # Adresse email pour recevoir l'URL du tunnel
+EMAIL = "votre_mail"  # Adresse email pour recevoir l'URL du tunnel
 SMTP_SERVER = "smtp.gmail.com"  # Serveur SMTP (exemple avec Gmail)
 SMTP_PORT = 465  # Port SMTP sécurisé (SSL)
-SMTP_USER = "pascal.paquet@gmail.com"  # Votre adresse Gmail (ou autre fournisseur)
-SMTP_PASSWORD = "wlubhwfcogtdfyfb"  # Mot de passe ou App Password (si Gmail)
+SMTP_USER = "votre_mail"  # Votre adresse Gmail (ou autre fournisseur)
+SMTP_PASSWORD = "votre_password"  # Mot de passe ou App Password (si Gmail)
 SUBDOMAIN = None  # Sous-domaine souhaité (None pour un sous-domaine aléatoire)
 
