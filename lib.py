787338c333da2c585205b4b1a37c025b2275f68f
Pascal <55432858+F4ICR@users.noreply.github.com>
2025-01-10 15:34:20 +0100

    Update lib.py

--- lib.py
+++ lib.py
@@ -264,6 +264,9 @@ def log_tunnel_change(previous_url, new_url):
 
 # Fonction pour envoyer un email avec l'URL du tunnel
 def send_email(tunnel_url):
+if not EMAIL_NOTIFICATIONS:
+        logger.info("L'envoi des emails est désactivé.")
+        return
     msg = MIMEText(f"Le tunnel est accessible à l'adresse suivante : {tunnel_url}")
     msg["Subject"] = "Localtunnel URL"
     msg["From"] = SMTP_USER
